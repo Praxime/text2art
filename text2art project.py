6ea5adc0739a7a11ee219b4c6ec20c7c5667b7e4
Praxime <159715275+Praxime@users.noreply.github.com>
2024-03-07 20:40:14 +0330

    Added Project

---
+++ text2art project.py
@@ -0,0 +1,57 @@
+# Simple console-code by Praxime 
+
+from os import system
+system('pip install art')
+system('cls')
+
+from termcolor import colored
+
+import time
+
+from art import *
+
+print(colored('To have better exprience, run this as orginial Python.', ('cyan')))
+print('')
+print(colored('Please wait...', ('light_yellow')))
+
+time.sleep(3)
+
+system('cls')
+
+text = ("Example text")
+
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+tprint(text,font='random')
+print("")
+print("")
+
+print(colored('You have 60 seconds to copy any font you want!', ('yellow')))
+time.sleep(61)
+print(colored('Bye! Code by Praxime', ('green')))
+time.sleep(1)
